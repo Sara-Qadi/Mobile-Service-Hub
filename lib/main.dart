f3908e991ad255a04d28a09f0b9a8508f4cae246
Sara-Qadi <sara.2002.qado@gmail.com>
2025-05-04 14:31:32 +0300

    profiles ui

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'create account App',
-      home: ForgotPasswordScreen(),
+      home: CustomerProfilePage(),
       debugShowCheckedModeBanner: false,
     );
   }
